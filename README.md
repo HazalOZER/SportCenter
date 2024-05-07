41886bd1bc274d619429c2d79e83132ef6b81e3d
Hazal ÖZER <hazalozer07@gmail.com>
2024-05-07 11:50:04 +0300

    readme added

---
+++ README.md
@@ -0,0 +1,40 @@
+# SportCenter
+
+SportCenter, spor merkezi yönetimi için basit bir web uygulamasıdır. Patika+ Fullstack Web Developer programı kapmasında hazırlanmıştır.
+
+
+### Kullanılan teknolojiler
+
+- Node.js
+- Vite.js
+- Bootsrap
+
+### Özellikler
+
+- Spor Salonunda bulunan dersler hakkında bilgilendirme sayfası
+- BMI Calculator ile VKE ölçümü yapılması
+- Eğiticiler hakkında bilgilendirme
+- Satışa sunulan ürünler görüntülenebiliyor
+- Müşteri yorumların bulunduğu alan 
+- Sport merkezi ile iletişime geçilebileceği iletişim sayfası
+
+### Görünüm
+
+#### Genel bilgilendirme
+![SportCenter](./images/gorunum1.png)
+
+#### Dersler ve VKE ölçüm alanı
+![SportCenter](./images/gorunum2.png)
+
+#### Eğitmenler
+![SportCenter](./images/gorunum3.png)
+
+#### Alışveriş alanı ve müşteri yorumları
+![SportCenter](./images/gorunum4.png)
+
+
+#### İletişim
+![SportCenter](./images/gorunum5.png)
+
+
+
--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 let yogaBtn = document.querySelector("#yoga");
 let groupBtn = document.querySelector("#group");
 let soloBtn = document.querySelector("#solo");
-let strerchingBtn = document.querySelector("#strerching");
+let strerchingBtn = document.querySelector("#stretching");
 let title = document.querySelector("#classesHeader1");
 let title2 = document.querySelector("#classesHeader2");
 let image = document.querySelector("#classesImg");
@@ -10,6 +10,10 @@ let header = document.querySelector("#header");
 let weight = document.querySelector("#weight");
 let height = document.querySelector("#height");
 let triangle = document.querySelector(".triangle");
+let yogaPoint = document.querySelector("#yogaPoint");
+let groupPoint = document.querySelector("#groupPoint");
+let soloPoint = document.querySelector("#soloPoint");
+let strerchingPoint = document.querySelector("#stretchPoint");
 
 window.addEventListener("scroll", (e) => {
     let scroll = window.scrollY;
@@ -20,6 +24,20 @@ window.addEventListener("scroll", (e) => {
     }
 });
 
+window.onload = ()=> {
+  image.src="./images/yoga.jpg";
+  title.innerHTML = "Why are your Yoga ?";
+  title2.innerHTML = "When comes Yoga Your Time";
+  yogaBtn.style.backgroundColor = "rgb(243, 157, 10)"; 
+  groupBtn.style.backgroundColor = "rgb(50, 84, 147)";
+  soloBtn.style.backgroundColor = "rgb(50, 84, 147)";
+  strerchingBtn.style.backgroundColor = "rgb(50, 84, 147)";
+  yogaPoint.style.display = "block";
+  groupPoint.style.display = "none";
+  soloPoint.style.display = "none";
+  strerchingPoint.style.display = "none";
+}
+
 yogaBtn.addEventListener("click", ()=>{
     image.src="./images/yoga.jpg";
     title.innerHTML = "Why are your Yoga ?";
@@ -28,8 +46,13 @@ yogaBtn.addEventListener("click", ()=>{
     groupBtn.style.backgroundColor = "rgb(50, 84, 147)";
     soloBtn.style.backgroundColor = "rgb(50, 84, 147)";
     strerchingBtn.style.backgroundColor = "rgb(50, 84, 147)";
+    yogaPoint.style.display = "block";
+    groupPoint.style.display = "none";
+    soloPoint.style.display = "none";
+    strerchingPoint.style.display = "none";
 
 });
+
 
 
 groupBtn.addEventListener("click", ()=> {
@@ -40,6 +63,10 @@ groupBtn.addEventListener("click", ()=> {
     yogaBtn.style.backgroundColor = "rgb(50, 84, 147)";
     soloBtn.style.backgroundColor = "rgb(50, 84, 147)";
     strerchingBtn.style.backgroundColor = "rgb(50, 84, 147)";
+    yogaPoint.style.display = "none";
+    groupPoint.style.display = "block";
+    soloPoint.style.display = "none";
+    strerchingPoint.style.display = "none";
 });
 
 soloBtn.addEventListener("click", ()=> {
@@ -50,6 +77,11 @@ soloBtn.addEventListener("click", ()=> {
     groupBtn.style.backgroundColor = "rgb(50, 84, 147)";
     yogaBtn.style.backgroundColor = "rgb(50, 84, 147)";
     strerchingBtn.style.backgroundColor = "rgb(50, 84, 147)";
+    yogaPoint.style.display = "none";
+    groupPoint.style.display = "none";
+    soloPoint.style.display = "block";
+    strerchingPoint.style.display = "none";
+
 });
 
 strerchingBtn.addEventListener("click", ()=> {
@@ -60,8 +92,12 @@ strerchingBtn.addEventListener("click", ()=> {
     groupBtn.style.backgroundColor = "rgb(50, 84, 147)";
     soloBtn.style.backgroundColor = "rgb(50, 84, 147)";
     yogaBtn.style.backgroundColor = "rgb(50, 84, 147)";
-});
 
+    yogaPoint.style.display = "none";
+    groupPoint.style.display = "none";
+    soloPoint.style.display = "none";
+    strerchingPoint.style.display = "block";
+});
 
 
 
@@ -69,37 +105,46 @@ strerchingBtn.addEventListener("click", ()=> {
 weight.addEventListener("input", (e) => {
     let heightMeter = parseFloat(height.value) / 100;
     let bmiValue = parseFloat(weight.value) / (heightMeter * heightMeter);
+  
 
     if (bmiValue > 35) {
-        triangle.style.left = "570px";
-        console.log(bmiValue);
-    } else if (bmiValue < 12) {
-        triangle.style.left = "100px";
-        console.log(bmiValue);
+       triangle.style.left = "85%";
+      console.log(bmiValue);
+    }  else if (bmiValue >= 30 && bmiValue < 35) {
+      triangle.style.left = `${bmiValue * 1.96}%`;
+      console.log(bmiValue);
+    }else if (bmiValue >= 25 && bmiValue < 30) {
+      triangle.style.left = `${bmiValue * 1.78}%`;
+      console.log(bmiValue);
     } else if (bmiValue > 18.5 && bmiValue < 25) {
-        triangle.style.left = `${bmiValue * 1.6}%`;
-        console.log(bmiValue);
-    } else if (bmiValue >= 25 && bmiValue < 30) {
-        triangle.style.left = `${bmiValue * 1.78}%`;
-        console.log(bmiValue);
-    } else if (bmiValue >= 30 && bmiValue < 35) {
-        triangle.style.left = `${bmiValue * 1.96}%`;
-        console.log(bmiValue);
-    }
-});
+      triangle.style.left = `${bmiValue * 1.6}%`;
+      console.log(bmiValue);
+    } else if (bmiValue < 18.5) {
+      triangle.style.left = "%10";
+      console.log(bmiValue);
+    } 
+  });
 
 const navbar = document.querySelector("#h-nav");
 const hmbgrBtn = document.querySelector(".hmbgr");
 
-let lastClickedId = "";
 
 hmbgrBtn.addEventListener("click", () => {
+   
+   header.style.backgroundColor == "#355592" ? header.style.backgroundColor="transparent" :  header.style.backgroundColor= "#355592"  ;
    navbar.classList.toggle("d-none");
+   
+   if(!navbar.classList.contains("d-none") || (window.scrollY > 50)){
+    header.style.backgroundColor = "#355592";
+   }else {
+    header.style.backgroundColor = "transparent";
+   }
+
 });
 
-// window.addEventListener("resize", (e) => {
-//     let width = e.target.innerWidth;
-//     if (width > 577) {
-//         navbar.style.transform = "translateY(0%)";
-//     }
-// });
+window.addEventListener("resize", (e) => {
+    let width = e.target.innerWidth;
+    if (width > 577) {
+        navbar.classList.add("d-none");
+    }
+});
